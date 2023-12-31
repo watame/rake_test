f29eb9ed394e9c6e31b7b52fd18810b014f4b555
watame <pandora.border@gmail.com>
2023-12-31 17:06:01 +0900

    add rake helper

---
+++ spec/rake_helper.rb
@@ -0,0 +1,12 @@
+require 'rake'
+
+# See https://qiita.com/aeroastro/items/c97bd26ce8b8818b6bed
+RSpec.configure do |config|
+  config.before(:suite) do
+    Rails.application.load_tasks # Load all the tasks just as Rails does (`load 'Rakefile'` is another simple way)
+  end
+
+  config.before(:each) do
+    Rake.application.tasks.each(&:reenable) # Remove persistency between examples
+  end
+end
